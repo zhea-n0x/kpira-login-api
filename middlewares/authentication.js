065e0d10e4bea462c1beb6f6c196c24f4102743c
zhea-n0x <adamfrdsid@gmail.com>
2024-11-30 23:39:26 +0000

    move decrypt process to after checking token availability

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,13 +3,13 @@ const crypto = require('../utils/simpleCrypto');
 const tokenVerification = (req, res, next) => {
     const token = req.headers['x-myapikey'];
 
-    const decryptedToken = crypto.decrypt(token);
-
     if(!token){
         return res.status(403).json({
             message: "Please provide a token!"
         });
     }
+
+    const decryptedToken = crypto.decrypt(token);
 
     if(decryptedToken !== process.env.API_KEY){
         return res.status(403).json({
